3f1e27c95b58df75cc1cd9be236df207b53850e5
vadickpb <vadick1@gmail.com>
2022-09-12 10:27:52 -0500

    add tecnology page links

--- src/pages/tecnology/tecnology.component.jsx
+++ src/pages/tecnology/tecnology.component.jsx
@@ -1,16 +1,41 @@
 import { useMediaQuery } from 'react-responsive'
+import data from '../../data.json'
+
+import img_lg01 from '../../assets/technology/image-launch-vehicle-portrait.jpg'
+import img_lg02 from '../../assets/technology/image-space-capsule-portrait.jpg'
+import img_lg03 from '../../assets/technology/image-spaceport-portrait.jpg'
+
 
 import LargeImageTec from '../../assets/technology/image-launch-vehicle-portrait.jpg'
 import SmallImageTec from '../../assets/technology/image-launch-vehicle-landscape.jpg'
 import './tecnology.styles.scss'
+import { useState } from 'react'
 
 const Tecnology = () => {
 
-  const isDesktopOrMovile = useMediaQuery({
-    query: '(min-width: 1000px)'
-  })
+  const [currentTecnology, setCurrentTecnology] = useState(0)
+  const {name, description} = data.technology[currentTecnology]
 
-  let img = isDesktopOrMovile ? LargeImageTec : SmallImageTec
+  // const isDesktopOrMovile = useMediaQuery({
+  //   query: '(min-width: 1000px)'
+  // })
+
+  // console.log(data.technology[currentTecnology]);
+
+  // let img = isDesktopOrMovile ? LargeImageTec : SmallImageTec
+
+  const setImage = (nameImage) => {
+    switch (nameImage) {
+      case ('Launch vehicle'):
+        return img_lg01;
+      case ('Spaceport'):
+        return img_lg03;
+      case ('Space capsule'):
+        return img_lg02;
+      default:
+        return false;
+    }
+  }
   return (
     <div className="tecnology-container">
       <div className="tecnology-subtitle">
@@ -20,9 +45,24 @@ const Tecnology = () => {
       <div className="tecnology-info-image-container">
 
         <div className="tecnology-links">
-          <div className="tecnology-link active">1</div>
-          <div className="tecnology-link">2</div>
-          <div className="tecnology-link">3</div>
+          <button 
+            className={`tecnology-link ${currentTecnology === 0 && 'active'}`}
+            onClick={() => setCurrentTecnology(0)}
+          >
+              1
+          </button>
+          <button 
+            className={`tecnology-link ${currentTecnology === 1 && 'active'}`}
+            onClick={() => setCurrentTecnology(1)}
+          >
+              2
+          </button>
+          <button 
+            className={`tecnology-link ${currentTecnology === 2 && 'active'}`}
+            onClick={() => setCurrentTecnology(2)}
+          >
+            3
+          </button>
         </div>
 
         <div className="tecnology-info-container">
@@ -30,18 +70,15 @@ const Tecnology = () => {
             The terminology...
           </h5>
           <h1 className="tecnology-name">
-            Launch vehicle
+            {name}
           </h1>
           <p className="tecnology-text">
-            A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a
-            payload from Earth's surface to space, usually to Earth orbit or beyond. Our
-            WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall,
-            it's quite an awe-inspiring sight on the launch pad!.
+            {description}
           </p>
         </div>
 
         <div className="tecnology-image-container">
-          <img src={img} alt="" />
+          <img src={setImage(name)} alt="" />
         </div>
       </div>
 
